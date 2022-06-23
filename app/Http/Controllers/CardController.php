3f0909e5171b9f98335d6f93da4f4642a0b2e346
bishal221973 <bishalchaudhary2018bca@gmail.com>
2022-06-23 15:49:01 +0545

    paginate

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -10,7 +10,7 @@ class CardController extends Controller
     public function index(Cases $cases)
     {
         $allCases = Cases::with('partyDetail')->with('oppositParty')->with('informToParty')->with('caseType')->whereMonth('created_at', date('m'))
-        ->whereYear('created_at', date('Y'))->get();
+        ->whereYear('created_at', date('Y'))->paginate(10);
         // return json_decode($allCases);
         // return $allCases;
         return view('cases.manage', compact(['allCases','cases']));
