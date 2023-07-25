3be8746a7b02295cc56a07ed71b7996b19ea39b7
santoshkalukunda <santoshkalukunda@gmail.com>
2023-07-25 15:37:13 +0545

    refer by filter in report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,19 +11,21 @@ class ReportController extends Controller
     public function index(Request $request, Cases $cases)
     {
         $users = User::orderBy('name')->get();
-        $allCases = Cases::with('partyDetail')->with('oppositParty')->with('informToParty')->with('caseType')->paginate(100);
-        
-        if ($request->filled('start') && $request->filled('end')){
-            $allCases= $allCases->whereBetween('date', [$request->start, $request->end]);
+        $allCases = Cases::with('partyDetail')
+            ->with('oppositParty')
+            ->with('informToParty')
+            ->with('caseType')
+            ->paginate(100);
+
+        if ($request->filled('start') && $request->filled('end')) {
+            $allCases = $allCases->whereBetween('date', [$request->start, $request->end]);
         }
-        if($request->filled('status')){
-            
-            $allCases= $allCases->where('case_status',$request->status);
-           
+        if ($request->filled('status')) {
+            $allCases = $allCases->where('case_status', $request->status);
         }
         // if($request->filled('witness')){
         //     $witness= $request->witness;
-            
+
         //  $collection=collect($allCases);
         //  $data= $collection->firstWhere('id', '1');
         //  dd($data);
@@ -34,90 +36,113 @@ class ReportController extends Controller
         // if($request->filled('search')){
         //     return "mannual search";
         // }
-        
+
         // $allCases = Cases::with('partyDetail')->with('oppositParty')->with('informToParty')->with('caseType')->paginate(10);
-       
-        return view('report.index', compact(['allCases','cases','users']));
+
+        return view('report.index', compact(['allCases', 'cases', 'users']));
     }
 
-    public function search(Request $request,Cases $cases)
+    public function search(Request $request, Cases $cases)
     {
-        
         $users = User::orderBy('name')->get();
 
         // $allCases=Cases::with('partyDetail')->with('oppositParty')->with('informToParty')->with('caseType')->where('case_number',$request->case_data)->orWhere('case_status',$request->case_data)->get();
         // return $cases;
-        $allCases = Cases::with('partyDetail')->with('oppositParty')->with('informToParty')
-        ->with('caseType')
-        ->when($request->filled('search'), function ($query) {
-            $query->where('case_number', request('search'))
-                ->orWhere('case_status', request('search'))
-                ->orWhereHas('partyDetail', function ($q) {
-                    $q->where('first_name','like','%'. request('search').'%')
-                        ->orWhere('last_name','like','%'. request('search').'%');
-                })
-                ->orWhereHas('oppositParty', function ($q) {
-                    $q->where('first_name','like','%'. request('search').'%')
-                        ->orWhere('last_name','like','%'. request('search').'%');
-                })
-                ->orWhereHas('informToParty', function ($q) {
-                    $q->where('relation','like','%'. request('search').'%');
-                })
-                ->orWhereHas('caseType', function ($q) {
-                    $q->where('case_type','like','%'. request('search').'%');
-                })
-                ->orWhereHas('userId', function ($q) {
-                    $q->where('user_id', request('user_id'));
-                });
-            // ->orWhere('')
-        })
-        // ->where('case_number',$request->case_data)
-        // ->orWhere('case_status',$request->case_data)
-        ->get();
+        $allCases = Cases::with('partyDetail')
+            ->with('oppositParty')
+            ->with('informToParty')
+            ->with('caseType')
+            ->when($request->filled('search'), function ($query) {
+                $query
+                    ->where('case_number', request('search'))
+                    ->orWhere('case_status', request('search'))
+                    ->orWhereHas('partyDetail', function ($q) {
+                        $q->where('first_name', 'like', '%' . request('search') . '%')->orWhere('last_name', 'like', '%' . request('search') . '%');
+                    })
+                    ->orWhereHas('oppositParty', function ($q) {
+                        $q->where('first_name', 'like', '%' . request('search') . '%')->orWhere('last_name', 'like', '%' . request('search') . '%');
+                    })
+                    ->orWhereHas('informToParty', function ($q) {
+                        $q->where('relation', 'like', '%' . request('search') . '%');
+                    })
+                    ->orWhereHas('caseType', function ($q) {
+                        $q->where('case_type', 'like', '%' . request('search') . '%');
+                    })
+                    ->orWhereHas('userId', function ($q) {
+                        $q->where('user_id', request('user_id'));
+                    });
+                // ->orWhere('')
+            })
+            // ->where('case_number',$request->case_data)
+            // ->orWhere('case_status',$request->case_data)
+            ->get();
         // return $allCases;
-        return view('report.index', compact(['allCases','cases','users']));
-    }
-    
-    public function dateFilter(Request $request,Cases $cases)
-    {
-        $allCases=Cases::with('partyDetail')->with('oppositParty')->with('informToParty')->with('caseType')->whereBetween('date', [$request->start, $request->end])->paginate(10);
-        // return $cases;
-        return view('report.index', compact(['allCases','cases']));
+        return view('report.index', compact(['allCases', 'cases', 'users']));
     }
 
-    public function caseStatus($key,Cases $cases)
-    {
-        $allCases=Cases::with('partyDetail')->with('oppositParty')->with('informToParty')->with('caseType')->where('case_status', $key)->paginate(10);
-        return view('report.index', compact(['allCases','cases']));
-    }
-    
-
-    public function witness($witness,Cases $cases)
-    {
-        $allCases = Cases::with('partyDetail')->with('oppositParty')->with('informToParty')
-        ->with('caseType')
-        ->WhereHas('informToParty', function ($q) use($witness){
-            $q->where('heir_name',$witness);
-        })->get();
-
-        return view('report.index', compact(['allCases','cases']));
-    }
-    public function caseType($type,Cases $cases)
-    {
-        $allCases = Cases::with('partyDetail')->with('oppositParty')->with('informToParty')
-        ->with('caseType')
-        ->WhereHas('caseType', function ($q) use($type){
-            $q->where('case_type',$type);
-        })->get();
-
-        return view('report.index', compact(['allCases','cases']));
-    }
-    public function UserId($userId,Cases $cases)
+    public function dateFilter(Request $request, Cases $cases)
     {
         $users = User::orderBy('name')->get();
 
-        $allCases = Cases::where('user_id',$userId)->get();
+        $allCases = Cases::with('partyDetail')
+            ->with('oppositParty')
+            ->with('informToParty')
+            ->with('caseType')
+            ->whereBetween('date', [$request->start, $request->end])
+            ->paginate(10);
+        // return $cases;
+        return view('report.index', compact(['allCases', 'cases', 'users']));
+    }
 
-        return view('report.index', compact(['allCases','cases','users']));
+    public function caseStatus($key, Cases $cases)
+    {
+        $users = User::orderBy('name')->get();
+
+        $allCases = Cases::with('partyDetail')
+            ->with('oppositParty')
+            ->with('informToParty')
+            ->with('caseType')
+            ->where('case_status', $key)
+            ->paginate(10);
+        return view('report.index', compact(['allCases', 'cases', 'users']));
+    }
+
+    public function witness($witness, Cases $cases)
+    {
+        $users = User::orderBy('name')->get();
+
+        $allCases = Cases::with('partyDetail')
+            ->with('oppositParty')
+            ->with('informToParty')
+            ->with('caseType')
+            ->WhereHas('informToParty', function ($q) use ($witness) {
+                $q->where('heir_name', $witness);
+            })
+            ->get();
+
+        return view('report.index', compact(['allCases', 'cases', 'users']));
+    }
+    public function caseType($type, Cases $cases)
+    {
+        $users = User::orderBy('name')->get();
+
+        $allCases = Cases::with('partyDetail')
+            ->with('oppositParty')
+            ->with('informToParty')
+            ->with('caseType')
+            ->WhereHas('caseType', function ($q) use ($type) {
+                $q->where('case_type', $type);
+            })
+            ->get();
+
+        return view('report.index', compact(['allCases', 'cases', 'users']));
+    }
+    public function UserId($userId, Cases $cases)
+    {
+        $users = User::orderBy('name')->get();
+
+        $allCases = Cases::where('user_id', $userId)->get();
+
+        return view('report.index', compact(['allCases', 'cases', 'users']));
     }
 }
